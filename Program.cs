3166f5f3dd38798fadd0b3d7094398d4c55ab360
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 22:48:44 +0530

    Main Method

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Add(10);
+            tree.Add(8);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(12);
+            tree.Add(17);
+
+            tree.TraverseTree();
+        }
+    }
+}

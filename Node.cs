233679f10746af814fd62c6654363fed7d9ba8bb
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 22:50:04 +0530

    Create node

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public Node Leftnode { get; set; }
+        public Node Rightnode { get; set; }
+        public int Data { get; set;}
+
+    }
+}
